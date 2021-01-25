441c34f0584a24363db004c5b88ae260a21c7986
Victoria Filileeva <onexsan@gmail.com>
2021-01-25 22:59:06 +0300

    test commit (minor text fix)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello wуrпупld!пуп
+          Hello world! It's React.js ;)
         </p>
 
       </header>
