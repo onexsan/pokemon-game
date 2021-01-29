97699e6d4e9a60c3dc12c146b0216a3f2dbd7afe
Victoria Filileeva <onexsan@gmail.com>
2021-01-29 14:07:07 +0300

    Homework 2: minor fixes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Header from './components/Header';
 import Layout from './components/Layout';
 import Footer from './components/Footer';
+import Background from './assets/bg3.jpg';
 
 const App = () => {
   return (
     <>
       <Header title="Header title" descr="Header description" />
-      <Layout id="my-prop-id" title="Layout title 1" descr="Layout description 1" urlBg />
+      <Layout id="my-prop-id" title="Layout title 1" descr="Layout description 1" urlBg={Background} />
       <Layout id="my-prop-id" title="Layout title 2" descr="Layout description 2" colorBg="#EFFBB5" />
-      <Layout id="my-prop-id" title="Layout title 3" descr="Layout description 3" urlBg />
+      <Layout id="my-prop-id" title="Layout title 3" descr="Layout description 3" urlBg={Background} />
       <Footer />
     </>
   );
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import s from './Layout.module.css';
-import Background from '../../assets/bg3.jpg';
 
-const Layout = (props) => {
-  const backgroundColor = props.colorBg ? { backgroundColor: props.colorBg } : {};
-  const backgroundImg = props.urlBg ? { backgroundImage: `url(${Background})` } : {};
+const Layout = ({ id, title, descr, urlBg, colorBg }) => {
+  const style = {};
+  if (urlBg) { style.backgroundImage = `url(${urlBg})` };
+  if (colorBg) { style.backgroundColor = colorBg };
 
   return (
-    <section className={s.root} id={props.id} style={{ ...backgroundColor, ...backgroundImg }} >
+    <section className={s.root} id={id} style={style} >
       <div className={s.wrapper}>
         <article>
           <div className={s.title}>
-            {props.title && <h3>{props.title}</h3>}
+            {title && <h3>{title}</h3>}
             <span className={s.separator}></span>
           </div>
           <div className={`${s.desc} ${s.full}`}>
-            {props.descr && <p>{props.descr}</p>}
+            {descr && <p>{descr}</p>}
           </div>
         </article>
       </div>
